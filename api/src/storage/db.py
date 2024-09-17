de9840838255887e2355164806640ffcbee88f47
Lucas Nakajima <nakajima.lucas02@gmail.com>
2024-09-17 16:07:46 +0100

    Fix rota aprovados- filtro de data

--- api/src/storage/db.py
+++ api/src/storage/db.py
@@ -41,7 +41,7 @@ def get_db_credentials():
     secret = secrets_manager.get_secret_value(SecretId=secret_arn)
 
     return {
-        "host": 'rds-pcd-prod.cluster-c4irymq85uhb.sa-east-1.rds.amazonaws.com',
+        "host": 'rds-dev-test-cluster.cluster-c4irymq85uhb.sa-east-1.rds.amazonaws.com',
         "user": json.loads(secret['SecretString'])['username'],
         "password": json.loads(secret['SecretString'])['password'],
     }
@@ -783,7 +783,7 @@ def get_aprovados_pcd(filters: dict) -> List[ApprovedRequest]:
         condition += f" and DATE(CONVERT_TZ(a.{filters['orientation_date']}, '+00:00', '-04:00')) >= %s"
         params.append(filters['start_date'])
     if filters.get('end_date'):
-        condition += f" and DATE(CONVERT_TZ(a.{filters['orientation_date']}, '+00:00', '-04:00')) >= %s"
+        condition += f" and DATE(CONVERT_TZ(a.{filters['orientation_date']}, '+00:00', '-04:00')) <= %s"
         params.append(filters['end_date'])
 
     params.append(filters['fim'])
@@ -827,7 +827,7 @@ def get_aprovados_ciptea(filters: dict) -> List[ApprovedRequest]:
         condition += f" and DATE(CONVERT_TZ(a.{filters['orientation_date']}, '+00:00', '-04:00')) >= %s"
         params.append(filters['start_date'])
     if filters.get('end_date'):
-        condition += f" and DATE(CONVERT_TZ(a.{filters['orientation_date']}, '+00:00', '-04:00')) >= %s"
+        condition += f" and DATE(CONVERT_TZ(a.{filters['orientation_date']}, '+00:00', '-04:00')) <= %s"
         params.append(filters['end_date'])
         
     params.append(filters['fim'])
