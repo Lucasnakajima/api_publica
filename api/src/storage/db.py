2fe13820badfb92325448e8c2a6f2c1dee1be8a7
ThiagoRoq <thiago.roque@sasi.com.br>
2025-02-07 09:57:45 -0400

    Update db.py

--- api/src/storage/db.py
+++ api/src/storage/db.py
@@ -318,8 +318,10 @@ def get_hash(filters: dict) -> List[HashRequest]:
         condition += 'and lower(TRIM(tipo_da_deficiencia_meta)) like %s'
         params.append('%'+ filters['deficiencia'] + '%') 
     if filters.get('recurso'):
-        condition += 'and tag_recurso = %s'
-        params.append(filters['recurso'])
+        if filters['recurso'] == 0:
+            condition += 'and tag_recurso < 1'
+        elif filters['recurso'] == 1:
+            condition += 'and tag_recurso > 0'
     if filters.get('start_date'):
         condition_group += " and MAX(DATE(CONVERT_TZ(created_at, '+00:00', '-04:00'))) >= %s "
         params.append(filters['start_date'])
